e6bff7fac03350881f81ab391c8673b98be98a68
MFauzan29 <mfauzan2905@gmail.com>
2025-07-04 15:55:34 +0700

    Finishing 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// App.jsx
-
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { Sun, Moon } from "lucide-react";
 
 // Import komponen dan hooks dari react-router-dom
 import { Routes, Route, Link } from "react-router-dom"; // <--- HANYA SATU KALI IMPORT INI
@@ -43,17 +42,32 @@ function App() {
     };
   }, []);
 
+  const [darkMode, setDarkMode] = useState(
+    () => localStorage.getItem("theme") === "dark"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (darkMode) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
   return (
     // Ganti <body> dengan <div> di sini. Kelas-kelas dari <body> dipindahkan ke <div> ini.
     <div className="min-h-screen bg-slate-50 dark:bg-black dark:text-white overflow-x-hidden">
       {/* HEADER ADA DI LUAR <Routes> AGAR SELALU TERLIHAT */}
       <header
-        className="fixed top-0 left-0 w-full z-50 text-black"
+        className="fixed top-0 left-0 w-full z-50 text-black opacity-100"
         style={{
           backgroundColor: `rgba(25,35,49, ${bgOpacity})`, // Menggunakan state bgOpacity di sini
         }}
       >
-        <section className="mx-auto flex max-w-4xl items-center justify-between p-4 ">
+        <section className="mx-auto flex max-w-5xl items-center justify-between p-4 ">
           <h1 className="text-2xl font-medium md:text-3xl">
             <ScrollLink
               to="home"
@@ -62,58 +76,92 @@ function App() {
               offset={-80}
               className="font-bold text-white cursor-pointer"
             >
-              🚀Muhamad Fauzan
+              <span className="text-yellow-500">about</span>MuhamadFauzan
             </ScrollLink>
           </h1>
 
-          {/* Toggle Button (Only on Mobile) */}
-          <button
-            id="mobile-menu-button"
-            className="text-3xl md:hidden focus:outline-none text-white"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            &#9776;
-          </button>
+          <div className="md:hidden flex items-center space-x-4 h-10">
+            {/* Dark Mode Toggle */}
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              className={`w-8 h-8 flex items-center justify-center transition-transform duration-500 ${
+                darkMode ? "rotate-180" : "rotate-0"
+              }`}
+              aria-label="Toggle Dark Mode"
+            >
+              {darkMode ? (
+                <Sun className="w-7 h-7 md:w-20 md:h-20  text-white" />
+              ) : (
+                <Moon className="w-7 h-7 text-white" />
+              )}
+            </button>
 
-          {/* Desktop Nav */}
-          <nav className="hidden md:flex space-x-8 text-lg bg-amber-100 px-6 py-3 rounded-full shadow-md whitespace-nowrap transition duration-300">
-            <ScrollLink
-              to="home"
-              smooth={true}
-              duration={600}
-              offset={-80}
-              className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+            {/* Hamburger Menu (Mobile only) */}
+            <button
+              id="mobile-menu-button"
+              className={`text-3xl md:hidden focus:outline-none text-white transition-transform duration-500 flex items-center justify-center ${
+                isMenuOpen ? "rotate-90" : "rotate-0"
+              }`}
+              onClick={() => setIsMenuOpen(!isMenuOpen)}
             >
-              Home
-            </ScrollLink>
-            <ScrollLink
-              to="aboutme"
-              smooth={true}
-              duration={600}
-              offset={-80}
-              className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+              &#9776;
+            </button>
+          </div>
+
+          <div className="hidden md:flex items-center space-x-4">
+            <button
+              onClick={() => setDarkMode(!darkMode)}
+              className={`w-8 h-8 flex items-center justify-center transition-transform duration-500 ${
+                darkMode ? "rotate-180" : "rotate-0"
+              }`}
+              aria-label="Toggle Dark Mode"
             >
-              About Me
-            </ScrollLink>
-            <ScrollLink
-              to="myworks"
-              smooth={true}
-              duration={600}
-              offset={-80}
-              className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
-            >
-              My Works
-            </ScrollLink>
-            <ScrollLink
-              to="contact"
-              smooth={true}
-              duration={600}
-              offset={-80}
-              className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
-            >
-              Contact Me
-            </ScrollLink>
-          </nav>
+              {darkMode ? (
+                <Sun className="w-7 h-7 md:w-20 md:h-20  text-white" />
+              ) : (
+                <Moon className="w-7 h-7 text-white" />
+              )}
+            </button>
+            {/* Desktop Nav */}
+            <nav className="hidden md:flex space-x-8 text-lg bg-amber-100 px-6 py-3 rounded-full shadow-md whitespace-nowrap transition duration-300">
+              <ScrollLink
+                to="home"
+                smooth={true}
+                duration={600}
+                offset={-80}
+                className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+              >
+                Home
+              </ScrollLink>
+              <ScrollLink
+                to="aboutme"
+                smooth={true}
+                duration={600}
+                offset={-80}
+                className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+              >
+                About Me
+              </ScrollLink>
+              <ScrollLink
+                to="myworks"
+                smooth={true}
+                duration={600}
+                offset={-80}
+                className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+              >
+                My Works
+              </ScrollLink>
+              <ScrollLink
+                to="contact"
+                smooth={true}
+                duration={600}
+                offset={-80}
+                className="cursor-pointer hover:bg-amber-500 px-3 py-1 rounded-full font-medium"
+              >
+                Contact Me
+              </ScrollLink>
+            </nav>
+          </div>
         </section>
 
         <div
@@ -151,10 +199,10 @@ function App() {
 
       {/* FOOTER ADA DI LUAR <Routes> AGAR SELALU TERLIHAT */}
       <footer className="bg-slate-900 dark:bg-black text-white py-12 px-6">
-        <div className="max-w-4xl mx-auto flex flex-col items-center">
+        <div className="max-w-4xl mx-auto flex flex-col items-center ">
           {/* Brand/Logo and Email */}
           <div className="text-center mb-6">
-            <h2 className="text-4xl font-bold font-serif mb-2">
+            <h2 className="text-4xl font-bold font-serif mb-2 ">
               <span className="text-yellow-500">about</span>Muhamad Fauzan
             </h2>
             <p className="text-md text-slate-400 flex items-center justify-center">
